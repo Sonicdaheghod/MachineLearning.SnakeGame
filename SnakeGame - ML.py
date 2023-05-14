a5f114796ceada00e7ee349447b804f3c09b3c51
Megan Tran <68253811+Sonicdaheghod@users.noreply.github.com>
2023-05-13 21:04:26 -0400

    Update SnakeGame - ML.py

--- SnakeGame - ML.py
+++ SnakeGame - ML.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# # Machine Learning - Data of Students' Grades
+
+# ### Idea based off of Dataquest
+# ### https://youtu.be/Hr06nSA-qww
+
 # In[1]:
 
 
-#Credit to https://youtu.be/Hr06nSA-qww?t=1022
+#Bringing up initial data
 
 
 # In[2]:
@@ -13,209 +18,159 @@
 import pandas as pd
 
 
-# # Access Data
-
-# ### Accessed data issue resolved by adding "r" before quotes
-
 # In[3]:
 
 
-teams = pd.read_csv(r"C:\Users\Megan Tran\Desktop\Megan's USB\College\Code\Python\Machine Learning\teams.csv")
-teams
-
-
-# ## Keeping Certain Columns
-
-# In[4]:
-
-
-teams = teams[["team", "country", "year", "athletes","age","prev_medals", "medals"]]
-teams
-
-
-# ## Finding correlation
-# 
-# #### The greater the r value, the better a prediction can be made for said variable 
-
-# In[5]:
-
-
-teams.corr()["medals"]
-
-
-# ## Plotting data
-
-# In[6]:
-
-
-#a - correlation of athletes x medals
-
-
-# In[7]:
-
-
-import seaborn as sns
-sns.lmplot(x="athletes",y="medals",data=teams, fit_reg=True,) 
-
-#we want this
-sns.lmplot(x="athletes",y="medals",data=teams, fit_reg=True,ci=None) 
-
-
-# In[8]:
-
-
-#b - correlation of athletes x age
-
-
-# In[9]:
-
-
-sns.lmplot(x="athletes",y="age",data=teams, fit_reg=True,ci=None) 
-
-
-# ## Histogram of data athlete x medals
-
-# In[10]:
-
-
-teams.plot.hist(y="medals")
-
-
-# In[11]:
-
-
-#Here, the graph is not evenly balanced because there are a lot of medals earned in the 0-100 range.
-
-
-# ## Data Cleaning
-
-# In[12]:
-
-
-teams[teams.isnull().any(axis=1)]
-
-
-# In[13]:
-
-
-#remove rows that don't have data on medals
-
-
-# In[14]:
-
-
-teams = teams.dropna()
-teams
-
-
-# In[15]:
-
-
-#respect order of data for training since future data cannot be used for prediction
-
-
-# In[16]:
-
-
-train = teams[teams["year"] < 2012].copy()
-#this determines how well model does
-test = teams[teams["year"] >= 2012].copy()
-
-
-
-
-
-# In[17]:
-
-
-train.shape
-
-
-# In[18]:
-
-
-test.shape
-
-
-# In[19]:
-
-
-#above is an 80 / 20 split which is normal
-
-
-# ## Training our Model
-
-# In[20]:
-
-
-from sklearn.linear_model import LinearRegression
-
-reg = LinearRegression()
-
-
-# In[21]:
-
-
-#training regression model to predict medals using predictors from data set
-predictors = ["athletes", "prev_medals"]
-target = "medals"
-
-
-# In[22]:
-
-
-reg.fit(train[predictors], train[target])
-
-
-# In[23]:
-
-
-#using above model to make predictions, cannot pass actual values bc that would give the model the answers which does not train it
-predictions = reg.predict(test[predictors])
-
-
-# In[24]:
-
-
-predictions
-
-
-# In[25]:
-
-
-#from the predictions of the medals, the model outputs negative and decimal numbers.
-#this makes no sense bc you cannot win a part of a medal nor win a negative number of medals
-
-#fixing the results
+game = pd.read_csv(r"C:\Users\Megan Tran\Desktop\Megan's USB\College\Code\Python\Machine Learning\snakes_count_1000.csv")
+game
 
 
 # In[26]:
 
 
-test["predictions"] = predictions
-test
+#Data cleaning
+
+game = game.dropna()
+game
 
 
 # In[27]:
 
 
-#fix that if the predictions for medals is less than zero, that value will just be zero on display
+#having model predict final grades of students based on provided data
+from sklearn.linear_model import LinearRegression
 
-test.loc[test["predictions"] < 0, "predictions"] = 0
-
-
+reg = LinearRegression()
 
 
 # In[28]:
 
 
-test["predictions"] = test["predictions"].round()
+train = game[game["GameNumber"] < 900].copy()
+#this determines how well model does
+test = game[game["GameNumber"] >= 900].copy()
+
 
 
 # In[29]:
 
 
+train.shape
+
+
+# In[30]:
+
+
+test.shape
+
+#90:10 split
+
+
+# In[31]:
+
+
+predictors = ["GameNumber"]
+target = "GameLength"
+
+
+# In[32]:
+
+
+reg.fit(train[predictors], train[target])
+
+
+# In[33]:
+
+
+#Here the model predicts the length of the game for future games played based on the data provided
+
+
+# In[34]:
+
+
+predictions = reg.predict(test[predictors])
+predictions
+
+
+# In[47]:
+
+
+#properly placing the predictions into the csv file
+
+
+# In[48]:
+
+
+test["Predictions Game Length"] = predictions
 test
+
+
+# In[49]:
+
+
+#Round predicted times to two decimal points
+
+test["Predictions Game Length"] = test["Predictions Game Length"].round(2)
+test
+
+
+# In[38]:
+
+
+#Seeing if the predictions fit with the regression line
+
+
+# In[16]:
+
+
+import seaborn as sns
+
+#we want this
+sns.lmplot(x="GameNumber",y="GameLength",data=game, fit_reg=True,ci=None) 
+
+
+# In[17]:
+
+
+#weak negative correlation that prediction model is trying to follow
+
+
+# ## Measuring error
+
+# In[52]:
+
+
+from sklearn.metrics import mean_absolute_error
+
+my_error = mean_absolute_error(test["GameLength"], test["Predictions Game Length"])
+my_error = my_error.round(2)
+my_error
+
+
+# In[ ]:
+
+
+#this value shows that on average, the predictions were around 16.46 minutes from the actual time the game went on for   
+
+
+# In[54]:
+
+
+#to determine if the error is good or not, error < standard deviation
+
+game.describe()["GameLength"]
+
+#16.46 < 23.41, so error is good
+
+
+# In[57]:
+
+
+#to better understand our error value, let's see the error between the actual and predicted values for each game played
+
+error_games = (test["GameLength"] - predictions).abs()
+error_games = error_games.round(2)
+error_games
 
 
 # In[ ]:
